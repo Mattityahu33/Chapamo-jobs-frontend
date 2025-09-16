c5b4e931690eeba38170f6f813cef27ccf328033
Mattityahu33 <mathewskapumba.28@gmail.com>
2025-09-16 17:58:15 +0200

    Add basename to BrowserRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/hermetico/', // 👈 Make sure the trailing slash is included
+// 👈 Make sure the trailing slash is included
   plugins: [react()],
   server: {
     proxy: {
